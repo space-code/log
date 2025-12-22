2c32fc2d25dcdd7f43448993266cd3d7a47b6d63
Nikita Vasilev <nv3212@gmail.com>
2025-12-22 16:36:55 +0400

    refactoring: remove `nonisolated(unsafe)` (#39)

--- Sources/Log/Classes/Models/LogLevel.swift
+++ Sources/Log/Classes/Models/LogLevel.swift
@@ -8,7 +8,7 @@ import Foundation
 // MARK: - LogLevel
 
 /// A set that includes all log levels.
-public struct LogLevel: OptionSet {
+public struct LogLevel: OptionSet, Sendable {
     // MARK: Initialization
 
     /// Creates a new log level.
@@ -22,33 +22,19 @@ public struct LogLevel: OptionSet {
 
     public typealias RawValue = UInt
 
-    #if swift(>=6.0)
-        /// Creates a new default `.off` instance with a bitmask of `1 << 0`.
-        public nonisolated(unsafe) static let off = LogLevel(rawValue: offBitmask)
-        /// Creates a new default `.debug` instance with a bitmask of `1 << 1`.
-        public nonisolated(unsafe) static let debug = LogLevel(rawValue: debugBitmask)
-        /// Creates a new default `.info` instance with a bitmask of `1 << 2`.
-        public nonisolated(unsafe) static let info = LogLevel(rawValue: infoBitmask)
-        /// Creates a new default `.error` instance with a bitmask of `1 << 3`.
-        public nonisolated(unsafe) static let error = LogLevel(rawValue: errorBitmask)
-        /// Creates a new default `.fault` instance with a bitmask of `1 << 4`.
-        public nonisolated(unsafe) static let fault = LogLevel(rawValue: faultBitmask)
-        /// Creates a new default `.error` instance.
-        public nonisolated(unsafe) static let all = LogLevel(rawValue: allBitmask)
-    #else
-        /// Creates a new default `.off` instance with a bitmask of `1 << 0`.
-        public static let off = LogLevel(rawValue: offBitmask)
-        /// Creates a new default `.debug` instance with a bitmask of `1 << 1`.
-        public static let debug = LogLevel(rawValue: debugBitmask)
-        /// Creates a new default `.info` instance with a bitmask of `1 << 2`.
-        public static let info = LogLevel(rawValue: infoBitmask)
-        /// Creates a new default `.error` instance with a bitmask of `1 << 3`.
-        public static let error = LogLevel(rawValue: errorBitmask)
-        /// Creates a new default `.fault` instance with a bitmask of `1 << 4`.
-        public static let fault = LogLevel(rawValue: faultBitmask)
-        /// Creates a new default `.error` instance.
-        public static let all = LogLevel(rawValue: allBitmask)
-    #endif
+    /// Creates a new default `.off` instance with a bitmask of `1 << 0`.
+    public static let off = LogLevel(rawValue: offBitmask)
+    /// Creates a new default `.debug` instance with a bitmask of `1 << 1`.
+    public static let debug = LogLevel(rawValue: debugBitmask)
+    /// Creates a new default `.info` instance with a bitmask of `1 << 2`.
+    public static let info = LogLevel(rawValue: infoBitmask)
+    /// Creates a new default `.error` instance with a bitmask of `1 << 3`.
+    public static let error = LogLevel(rawValue: errorBitmask)
+    /// Creates a new default `.fault` instance with a bitmask of `1 << 4`.
+    public static let fault = LogLevel(rawValue: faultBitmask)
+    /// Creates a new default `.error` instance.
+    public static let all = LogLevel(rawValue: allBitmask)
+
     /// Returns the raw bitmask value of the LogLevel and satisfies the `RawRepresentable` protocol.
     public let rawValue: RawValue
 
